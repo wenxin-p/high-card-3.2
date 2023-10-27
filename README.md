aa5788d8c61564405ebb16992d1b5325a41c6ed0
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-27 14:20:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rocket Academy Coding Bootcamp: High Card
+
+## Available Scripts
+
+This project was created using with Vitejs. In the project directory, please run the following commands:
+
+npm install
+npm run dev
+
+The second command will run the app
+To see the rendered output please open http://localhost:5173 in the browser of your choice, EG: Google Chrome.
+
+The page will reload when you make changes.
+You may also see any lint errors in the console.
---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import { makeShuffledDeck } from "./utils.jsx";
+import { useState } from "react";
+
+function App(props) {
+  // Set default value of card deck to new shuffled deck
+  const [cardDeck, setCardDeck] = useState(makeShuffledDeck());
+  // currCards holds the cards from the current round
+  const [currCards, setCurrCards] = useState([]);
+
+  const dealCards = () => {
+    const newCurrCards = [cardDeck.pop(), cardDeck.pop()];
+    setCurrCards(newCurrCards);
+  };
+  // You can write JavaScript here, just don't try and set your state!
+
+  // You can access your current components state here, as indicated below
+  const currCardElems = currCards.map(({ name, suit }) => (
+    // Give each list element a unique key
+    <div key={`${name}${suit}`}>
+      {name} of {suit}
+    </div>
+  ));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>React High Card 🚀</h2>
+        {currCardElems}
+        <br />
+        <button onClick={dealCards}>Deal</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
